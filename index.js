aec56f02e1a34ed88e5be3f98e6261ae58b514e6
Andrejs <aumeister.a@gmail.com>
2021-08-17 14:16:03 +0300

    hardcoded fix for urls

--- index.js
+++ index.js
@@ -7,34 +7,35 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-const imageRoute = require("./routes/images")
-const cors = require('cors');
+const imageRoute = require("./routes/images");
+const cors = require("cors");
 
 dotenv.config();
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
+	process.env.MONGO_URL,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => {
+		console.log("Connected to MongoDB");
+	}
 );
 
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
-app.use(cors())
-
+app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-app.use("/api/images", imageRoute)
-
+app.use("/api/images", imageRoute);
+// hardcode fix
+app.use("/api/profile/images", imageRoute);
+app.use("/api/profile/users", userRoute);
 
 app.listen(8080, () => {
-  console.log("Backend server is running!");
+	console.log("Backend server is running!");
 });
